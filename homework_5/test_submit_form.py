cf9f05ae4b51c1534d4479799f478bb3f75db3f7
SKropotova <Kropotova.S.M@yandex.ru>
2025-08-22 15:31:39 +0300

    урок 5

--- homework_5/test_submit_form.py
+++ homework_5/test_submit_form.py
@@ -6,35 +6,35 @@ def test_submit_form():
     browser.open('/automation-practice-form')
     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test.png'))
 
-    # Заполнение основных данных
+
     browser.element('#firstName').type('София')
     browser.element('#lastName').type('Кропотова')
     browser.element('#userEmail').type('sofia@example.com')
     browser.element('#gender-radio-2').double_click()
     browser.element('#userNumber').type('79999999999')
 
-    # Дата рождения: 12 июня 1997
+
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').send_keys('June')
     browser.element('.react-datepicker__year-select').send_keys('1997')
     browser.element('.react-datepicker__day--012').click()
 
-    # Предметы и хобби
+
     browser.element('#subjectsInput').should(be.visible).type('Maths').press_enter()
     browser.element('[for=hobbies-checkbox-1]').click()
 
-    # Загрузка файла (условно, можно заменить путь)
+
     browser.element('#uploadPicture').send_keys(file_path)
 
-    # Адрес и выбор штата/города
+
     browser.element('#currentAddress').type('Нижний Новгород, ул. Королева, д. 4')
     browser.element('#state').click().element('#react-select-3-option-0').click()
     browser.element('#city').click().element('#react-select-4-option-0').click()
 
-    # Отправка формы
+
     browser.element('#submit').press_enter()
 
-    # Проверка
+
     browser.element('.modal-content').should(have.text('София Кропотова'))
     browser.element('.modal-content').should(have.text('sofia@example.com'))
     browser.element('.modal-content').should(have.text('Female'))
