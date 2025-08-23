6292397941738ddd87f865dc86849660688fc0f4
SKropotova <Kropotova.S.M@yandex.ru>
2025-08-23 10:38:47 +0300

    урок 9 fix

--- homeworh92/tests/test_submit_form.py
+++ homeworh92/tests/test_submit_form.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from homeworh92.pages.registration_page import RegistrationPage
 from homeworh92.models.user import User
 
--- homework_9/tests/test_submit_form.py
+++ homework_9/tests/test_submit_form.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from homework_9.pages.registration_page import RegistrationPage
 
 def test_fill_form():
