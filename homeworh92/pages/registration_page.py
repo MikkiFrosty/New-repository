f684e936beee9636edeabc277ce97236cfe5dc9c
SKropotova <Kropotova.S.M@yandex.ru>
2025-08-28 21:46:09 +0300

    Fix

--- homeworh92/pages/registration_page.py
+++ homeworh92/pages/registration_page.py
@@ -1,4 +1,6 @@
 import os
+
+import self
 from selene import browser, have, be
 from homeworh92.models.user import User
 from pathlib import Path
@@ -8,52 +10,52 @@ class RegistrationPage:
 
     def open_form(self):
         with allure.step("Переход на страницу"):
-        browser.open('https://demoqa.com/automation-practice-form')
+            browser.open('https://demoqa.com/automation-practice-form')
         return self
 
     def register(self, user: User):
         file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'resources', user.file_name))
         with allure.step("Заполнение имени"):
-        browser.element('#firstName').type(user.first_name)
+            browser.element('#firstName').type(user.first_name)
         with allure.step("Заполнение фамилии"):
-        browser.element('#lastName').type(user.last_name)
+            browser.element('#lastName').type(user.last_name)
         with allure.step("Заполнение емеила"):
-        browser.element('#userEmail').type(user.email)
+            browser.element('#userEmail').type(user.email)
         with allure.step("Выбор пола"):
-        browser.element(f'//label[text()="{user.gender}"]').click()
+            browser.element(f'//label[text()="{user.gender}"]').click()
         with allure.step("Заполнение номера телефона"):
-        browser.element('#userNumber').type(user.phone)
+            browser.element('#userNumber').type(user.phone)
 
         # birthday
         with allure.step("Заполнение даты рождения"):
-        browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').send_keys(user.month)
-        browser.element('.react-datepicker__year-select').send_keys(str(user.year))
-        browser.element(f'.react-datepicker__day--0{user.day:02}').click()
+            browser.element('#dateOfBirthInput').click()
+            browser.element('.react-datepicker__month-select').send_keys(user.month)
+            browser.element('.react-datepicker__year-select').send_keys(str(user.year))
+            browser.element(f'.react-datepicker__day--0{user.day:02}').click()
         with allure.step("Выбор предмета"):
-        browser.element('#subjectsInput').should(be.visible).type(user.subject).press_enter()
+            browser.element('#subjectsInput').should(be.visible).type(user.subject).press_enter()
         with allure.step("Выбор хобби"):
-        browser.element(f'//label[text()="{user.hobby}"]').click()
+            browser.element(f'//label[text()="{user.hobby}"]').click()
         with allure.step("Загрузка изображения"):
-        browser.element('#uploadPicture').send_keys(file_path)
+            browser.element('#uploadPicture').send_keys(file_path)
         with allure.step("Выбор адреса"):
-        browser.element('#currentAddress').type(user.address)
-        browser.element('#state').click().element(f'//div[text()="{user.state}"]').click()
-        browser.element('#city').click().element(f'//div[text()="{user.city}"]').click()
+            browser.element('#currentAddress').type(user.address)
+            browser.element('#state').click().element(f'//div[text()="{user.state}"]').click()
+            browser.element('#city').click().element(f'//div[text()="{user.city}"]').click()
         with allure.step("Отправка формы"):
-        browser.element('#submit').press_enter()
+            browser.element('#submit').press_enter()
         return self
 
     def should_have_registered(self, user: User):
         with allure.step("Проверка формы"):
-        modal = browser.element('.modal-content')
-        modal.should(have.text(f'{user.first_name} {user.last_name}'))
-        modal.should(have.text(user.email))
-        modal.should(have.text(user.gender))
-        modal.should(have.text(user.phone))
-        modal.should(have.text(f'{user.day} {user.month},{user.year}'))
-        modal.should(have.text(user.subject))
-        modal.should(have.text(user.hobby))
-        modal.should(have.text(user.file_name))
-        modal.should(have.text(user.address))
-        modal.should(have.text(f'{user.state} {user.city}'))
+            modal = browser.element('.modal-content')
+            modal.should(have.text(f'{user.first_name} {user.last_name}'))
+            modal.should(have.text(user.email))
+            modal.should(have.text(user.gender))
+            modal.should(have.text(user.phone))
+            modal.should(have.text(f'{user.day} {user.month},{user.year}'))
+            modal.should(have.text(user.subject))
+            modal.should(have.text(user.hobby))
+            modal.should(have.text(user.file_name))
+            modal.should(have.text(user.address))
+            modal.should(have.text(f'{user.state} {user.city}'))
