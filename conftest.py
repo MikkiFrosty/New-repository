5d55d353962894b2d0b9e238e6a237a4a16f07d0
SKropotova <Kropotova.S.M@yandex.ru>
2025-08-28 22:11:40 +0300

    Fix

--- conftest.py
+++ conftest.py
@@ -1,41 +1,38 @@
 import os
 import pytest
 from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import InvalidSessionIdException, WebDriverException
-from selene import browser
-from utils import attach
 
-load_dotenv()  # .env
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv(override=True)
 
 @pytest.fixture(scope='function', autouse=True)
 def remote_browser_setup():
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
     from selene.support.shared import browser
-    from selenium.common.exceptions import InvalidSessionIdException, WebDriverException
-    import os
 
-    login = os.getenv("SELENOID_LOGIN")
-    password = os.getenv("SELENOID_PASS")
-    host = os.getenv("SELENOID_URL", "").replace('http://','').replace('https://','').rstrip('/')
+    login = os.getenv('SELENOID_LOGIN')
+    password = os.getenv('SELENOID_PASS')
+    host = os.getenv('SELENOID_URL', '')
+
+    assert all([login, password, host]), f'ENV missing: login={bool(login)}, pass={bool(password)}, url={bool(host)}'
+
+    host = host.replace('http://','').replace('https://','').rstrip('/')
 
     options = Options()
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "128.0")
-    options.set_capability("selenoid:options", {"enableVNC": True, "enableVideo": True})
-    options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
+    options.set_capability('browserName', 'chrome')
+    options.set_capability('browserVersion', '128.0')
+    options.set_capability('selenoid:options', {'enableVNC': True, 'enableVideo': True})
+    options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@{host}/wd/hub",
+        command_executor=f'https://{login}:{password}@{host}/wd/hub',
         options=options
     )
+    print("DEBUG login:", os.getenv("SELENOID_LOGIN"))
+    print("DEBUG pass:", os.getenv("SELENOID_PASS"))
+    print("DEBUG url:", os.getenv("SELENOID_URL"))
     browser.config.driver = driver
-
     yield
-
-    try:
-        browser.quit()
-    except (InvalidSessionIdException, WebDriverException):
-        pass
+    browser.quit()
